e5db791a9bf0b6666c812647a958646dfe342995
Jerome Leclanche <jerome@leclan.ch>
2015-05-16 03:29:09 +0200

    Clean up the build file

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
-SCRIPT="$(readlink -f $0)"
-SCRIPTDIR=$(dirname "$SCRIPT")
-cd "$SCRIPTDIR"
+basedir=$(readlink -e $(dirname "$0"))
+protodir="$basedir/proto/"
 
-GENDIR="generated"
-FINAL_DIR="python/hsproto"
+
+##
+# Python protos
+
+outdir="$basedir/_python-generated"
+finaldir="$basedir/python/hsproto"
 
 # Create directories
-test -d "$GENDIR" && rm -rf "$GENDIR"
-test -d "$FINAL_DIR" && rm -rf "$FINAL_DIR"
-mkdir "$GENDIR"
+test -d "$outdir" && rm -rf "$outdir"
+test -d "$finaldir" && rm -rf "$finaldir"
+mkdir -p "$outdir"
 
-# Generating protobuf files
-find proto -name '*.proto' -exec protoc -Iproto "--python_out=$GENDIR" {} \+
+# Generating python protobuf files
+find "$protodir" -type f -name '*.proto' -exec protoc --proto_path="$protodir" --python_out="$outdir" {} \;
 
-# Fixing protobuf files
-find "$GENDIR" -name '*.py' -exec python3 python/relativize_module.py "$GENDIR" {} \+
+# Fix the generated protobuf files
+find "$outdir" -name '*.py' -exec python3 "$basedir/python/relativize_module.py" "$outdir" {} \+
 
 # Final move
-mv "$GENDIR" "$FINAL_DIR"
+mv "$outdir" "$finaldir"
